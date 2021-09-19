4367079d5a1f5999221426f49426b00afd9290ef
MildreAlfaro1286 <malfaro9@misena.edu.co>
2021-09-19 17:41:44 -0500

    clase..repaso array, localstorage y librerias externas.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('hola mundo')
 var misdatos = [];
 
+localStorage.setItem('prueba', 'leonadrian')
+
 var guardar = function(){
     var nombre2 = document.getElementById('nombre').value
     var apellido2 = document.getElementById('apellido').value;
@@ -8,14 +10,51 @@ var guardar = function(){
     misdatos.push({nombre:nombre2,apellido:apellido2})
     var x = JSON.stringify(misdatos)
     console.log(x)
-     localStorage.setItem('datos' ,x)
+    alert('prueba')
+    localStorage.setItem('datos' ,x)
     //localStorage.setItem('apellido' ,apellido)
 }
 var cargar = function(){
+    console.log('mildremildre')
+    swal("Hello world!");
+    swal({
+        title: "hola?",
+        text: "Mujeres Digitales",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("que tengas un excelente dia!!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
     //document.getElementById('nombre').value = localStorage.getItem('nombre')
     //document.getElementById('apellido').value =  localStorage.getItem('apellido')
 }
 
-misdatos = JSON.parse(localStorage.getItem('datos').toString())
+var existe = localStorage.getItem('datos')
+console.log(existe)
+if (existe != null){
+    console.log ('si entro')
+    misdatos=JSON.parse(localStorage.getItem('datos').toString())
+
+
+}
+
+var eliminarlocal = function(){
+    console.log('eliminando datos')
+    //localStorage.removeItem('datos')
+    localStorage.clear()
+
+
+}
+
+
+
 
 
